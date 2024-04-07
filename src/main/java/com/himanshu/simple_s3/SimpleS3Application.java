7b4d1b0ad78ddf9ff5222ebeb03686e277ed1e00
Himanshu Singh Tomar <himanshusinghtomar204@gmail.com>
2024-04-07 14:39:03 +0530

    s3

---
+++ src/main/java/com/himanshu/simple_s3/SimpleS3Application.java
@@ -0,0 +1,13 @@
+package com.himanshu.simple_s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleS3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleS3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/himanshu/simple_s3/SimpleS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himanshu.simple_s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleS3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
